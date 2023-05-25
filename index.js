e4845e8c044d90a6e960fcfc417a0eba7ff31a30
Amanmandal-M <amanmandal644@gmail.com>
2023-05-25 12:50:30 +0530

    Deploy in Railway

---
+++ index.js
@@ -0,0 +1,19 @@
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
+require('dotenv').config();
+
+server.use(middlewares);
+
+server.use(jsonServer.rewriter({
+    "/users/:id": "/users?id=:id",
+    "/appointments/:id": "/appointments?id=:id"
+}))
+
+server.use(router)
+server.use(jsonServer.bodyParser);
+
+server.listen(process.env.PORT, () => {
+  console.log('JSON Server is running')
+})
