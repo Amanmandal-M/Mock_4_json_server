7ac3951dc1c6a44f6c422975eac6880daa13f3d8
Amanmandal-M <amanmandal644@gmail.com>
2023-05-27 03:02:04 +0530

    Added cors

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const jsonServer = require('json-server')
+const cors = require('cors');
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
 require('dotenv').config();
 
+server.use(cors());
 server.use(middlewares);
 
 server.use(jsonServer.rewriter({
